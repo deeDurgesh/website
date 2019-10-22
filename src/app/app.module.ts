5248577fcf9f5a83c2e5cca15122547e17f956fc
wcssharma <ssharma@webcontentor.com>
2019-10-22 12:13:35 +0530

    dynamic-menu created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,23 @@ import { AppComponent } from './app.component';
 import { SideNavComponent } from './core/component/side-nav/side-nav.component';
 import { HeaderComponent } from './core/component/header/header.component';
 import { DashboardComponent } from './core/component/dashboard/dashboard.component';
-
+import { DynamicMenuComponent } from './core/component/dynamic-menu/dynamic-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideNavComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    DynamicMenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-   
+    BrowserAnimationsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/component/dynamic-menu/dynamic-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-menu',
+  templateUrl: './dynamic-menu.component.html',
+  styleUrls: ['./dynamic-menu.component.css']
+})
+export class DynamicMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
